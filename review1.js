91bd5a60bbedb0b4719eee255131da989705927f
wkennedy210@gmail.com <6560256wK1>
2020-07-20 23:11:43 -0400

    add review

---
+++ review1.js
@@ -0,0 +1,61 @@
+//1.  Create a function called fullName that takes in 2 parameters, firstName and lastName, and just returns you're full name in one string
+
+//2. Create a function called filterSports that will go through each item of the following array below and if the sport ends in "ball" return the string "${sport} is most likely an american sport" and if it does not end in "ball" return the string "${sport} is most likely a global sport"
+
+const sports = [
+  'basketball',
+  'football',
+  'tennis',
+  'soccer',
+  'baseball',
+  'rugby',
+  'volleyball',
+];
+
+//3. DOM MANIPULATION
+// query the DOM for the ul with the class 'celebrities'
+// create new 'li' tag and give it a class of 'actor'
+// add a 'p' tag inside the 'li' and provide the name of an actor
+// add the new li to the ul with the class of 'celebrities'
+
+//4. LETS LOOP THROUGH THIS ARRAY AND ADD `goes to wyncode` to EVERYONES NAME
+const firstNames = ['Francesca', 'Erialbania', 'Jorge', 'Ana'];
+const wyncode = 'wyncode';
+
+// 5. Console.log LeBron James from the following object.
+// 6. Console.log Chris Paul from the following object.
+const nba = {
+  teams: [
+    {
+      name: 'Miami Heat',
+      players: ['Dwyane Wade', 'Jimmy Butler', 'Tyler Herro', 'Bam Adebayo'],
+    },
+    {
+      name: 'LA Clippers',
+      players: [
+        'Kawhai Leonard',
+        'Paul George',
+        'Patrick Beverly',
+        'Montrez Harrell',
+      ],
+    },
+    {
+      name: 'LA Laker',
+      players: [
+        'Anthony Davis',
+        'Rajon Rondo',
+        'LeBron James',
+        'Dwight Howard',
+      ],
+    },
+    {
+      name: 'OKC Thunder',
+      players: [
+        'Chris Paul',
+        'Steven Adams',
+        'Shai Gilgeous-Alexander',
+        'Dennis Shroeder',
+      ],
+    },
+  ],
+};
